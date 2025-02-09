ca55c1ed735079254f295307611479022462520f
HelloPyunny <pyunhs0121@gmail.com>
2025-02-09 16:19:28 -0600

    scraper library loaded

---
+++ backend/scraping/scraper.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import json
+
+# Path to ChromeDriver
+chrome_path = "/Applications/Google Chrome.app"
+
+# 실시간 베스트 갤러리에서 게시글 목록 가져오기
+
+# 게시글 목록 가져오기
+
+# 데이터 저장
+
+# 드라이버 종료
